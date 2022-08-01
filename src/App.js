a3738753134357838d755ff421637a0f16c8aa02
Rita <ritahung19@gmail.com>
2022-08-01 23:32:25 +0800

    add state and toggleIsFavorite function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-function App() {
-  return (
-   <main>
-    <article className="card">
-      <img src="../images/user.png" alt="user" className="card-image"/>
-      <div className="card-info">
-        <img src="../images/star-empty.png" alt="star" className="empty-star"/>
-        <h2 className="card-name">cardname</h2>
-        <p className="card-contact">email</p>    
-        <p className="card-contact">09XX-XXXXX</p>    
-      </div>
-    </article>
+import { useState } from "react";
 
-   </main>
+function App() {
+  const [info, setInfo] = useState({
+    name: "John",
+    email: "123@yahoo.com.tw",
+    phone: "09XX-XXXXXX",
+    isFavorite: "false",
+  });
+
+  let starImage = info.isFavorite ? "star-filled.png" : "star-empty.png";
+
+  function toggleIsFavorite() {
+    setInfo((prevInfo) => ({ ...prevInfo, isFavorite: !prevInfo.isFavorite }));
+  }
+
+  return (
+    <main>
+      <article className="card">
+        <img src="../images/user.png" alt="user" className="card-image" />
+        <div className="card-info">
+          <img
+            src={`../images/${starImage}`}
+            alt="star"
+            className="empty-star"
+            onClick={toggleIsFavorite}
+          />
+          <h2 className="card-name">{info.name}</h2>
+          <p className="card-contact">{info.email}</p>
+          <p className="card-contact">{info.phone}</p>
+        </div>
+      </article>
+    </main>
   );
 }
 
